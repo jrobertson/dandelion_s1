c331fdbd579bc9001613cf31312204337afbbc79
James Robertson <james@ralina.(none)>
2014-02-10 10:22:40 +0000

    1st commit

---
+++ dandelion_s1.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'dandelion_s1'
+  s.version = '0.1.0'
+  s.summary = 'A kind of Rack-Rscript web server which facilitates static files, basic authentication, and private pages.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/**/*.rb']
+  s.add_dependency('rack-rscript')
+  s.signing_key = '../privatekeys/dandelion_s1.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@r0bertson.co.uk'
+  s.homepage = 'https://github.com/jrobertson/dandelion_s1'
+end
---
+++ lib/dandelion_s1.rb
@@ -0,0 +1,73 @@
+#!/usr/bin/env ruby
+
+# file: dandelion_s1.rb
+
+require 'rack-rscript'
+
+
+class DandelionS1 < RackRscript
+
+  def initialize(h={})
+
+    raw_opts = {root: 'www', access: {}, static: []}.merge h
+    #@static = %w(index.html dynarex snippets)
+    @static = raw_opts[:static]
+    @root = raw_opts[:root]
+
+    #@access_list = {'/do/r/hello3' => 'user'}
+    @access_list = raw_opts[:access]
+    super(raw_opts)
+  end
+
+  def call(e)
+
+    private_user = @access_list[e['REQUEST_PATH']]
+
+    if private_user.nil? or private_user == e['REMOTE_USER'] then
+      super(e)
+    else
+      request = '/unauthorised/'
+      content, content_type, status_code = run_route(request)        
+      content_type ||= 'text/html'
+      [status_code=401, {"Content-Type" => content_type}, [content]]
+    end
+
+  end
+
+  def default_routes(env, params)
+
+    super(env, params)
+
+    get /(\/(?:#{@static.join('|')}).*)/ do |path|
+
+      filepath = File.join(@root, path)
+
+      if path.length < 1 or path[-1] == '/' then
+        path += 'index.html' 
+        File.read File.join(base, path)
+      elsif File.directory? filepath then
+        Redirect.new (path + '/') 
+      elsif File.exists? filepath then
+        h = {xml: 'application/xml', html: 'text/html', png: 'image/png', 
+             jpg: 'image/jpeg', txt: 'text/plain'}
+
+        [File.read(filepath), h[filepath[/\w+$/].to_sym]]
+      else
+        'oops, file ' + filepath + ' not found'
+      end
+
+    end
+
+    get /^\/$/ do
+
+      file = File.join(@root, 'index.html')
+      File.read file
+    end
+
+    get '/unauthorised/' do
+      'unauthorised user'
+    end
+ 
+  end 
+
+end
