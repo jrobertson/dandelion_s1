47fc9fda5ace4d30c7917b0abc0bb9fce13701dd
James Robertson <james@jamesrobertson.me.uk>
2022-01-15 17:36:21 +0000

    bug fix: Now works with Ruby 3.0.2 and above.

--- dandelion_s1.gemspec
+++ dandelion_s1.gemspec
@@ -1,17 +1,17 @@
 Gem::Specification.new do |s|
   s.name = 'dandelion_s1'
-  s.version = '0.3.2'
+  s.version = '0.4.0'
   s.summary = 'A kind of Rack-Rscript web server which facilitates ' +
       'static files, cookie based authentication, and private pages.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/dandelion_s1.rb']
-  s.add_runtime_dependency('rack-rscript', '~> 1.1', '>=1.1.4')
-  s.add_runtime_dependency('simple-config', '~> 0.6', '>=0.6.4')
-  s.add_runtime_dependency('martile', '~> 1.1', '>=1.1.5')
+  s.add_runtime_dependency('rack-rscript', '~> 1.2', '>=1.2.3')
+  s.add_runtime_dependency('simple-config', '~> 0.7', '>=0.7.1')
+  s.add_runtime_dependency('martile', '~> 1.4', '>=1.4.6')
   s.signing_key = '../privatekeys/dandelion_s1.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/dandelion_s1'
-  s.required_ruby_version = '>= 2.1.2'
+  s.required_ruby_version = '>= 3.0.2'
 end
--- lib/dandelion_s1.rb
+++ lib/dandelion_s1.rb
@@ -12,47 +12,47 @@ class DandelionS1 < RackRscript
   def initialize(opts={})
 
     h = {root: 'www', static: [], passwords: {'user' => 'us3r'}}.merge(opts)
-    
+
     @passwords = h[:passwords]
     access_list = h[:access]
     @app_root = Dir.pwd
 
     #@access_list = {'/do/r/hello3' => 'user'}
-        
+
     if access_list then
-      
+
       h2 = SimpleConfig.new(access_list).to_h
       conf_access = h2[:body] || h2
       @access_list = conf_access.inject({}) \
                                   {|r,x| k,v = x; r.merge(k.to_s => v.split)}
-    
+
     end
-    
-    h3 = %i(log pkg_src rsc_host rsc_package_src root static debug)\
+
+    h3 = %i(log pkg_src rsc_host rsc root static debug)\
         .inject({}) {|r,x| r.merge(x => h[x])}
-    
-    super(h3)
-    @log.debug '@access_list: ' + @access_list.inspect if @log    
+
+    super(**h3)
+    @log.debug '@access_list: ' + @access_list.inspect if @log
     @log.debug 'end of initialize' if @log
-    
+
   end
 
   def call(e)
 
     request = e['REQUEST_PATH']
     @log.debug 'request: ' + request.inspect if @log
-    
+
     return super(e) if request == '/login'
     r = @access_list.detect {|k,v| request =~ Regexp.new(k)} if @access_list
     private_user = r ? r.last : nil
-    
+
     req = Rack::Request.new(e)
     user = req.session[:username]
 
-    #@log.debug 'user: ' + user.inspect if @log
+    @log.debug 'user: ' + user.inspect if @log
     #@log.debug '@e: ' + e.inspect if @log
     return jumpto '/login2?referer=' + e['PATH_INFO'] unless user
-    
+
     if private_user.nil? then
       super(e)
     elsif (private_user.is_a? String and private_user == user) \
@@ -63,78 +63,78 @@ class DandelionS1 < RackRscript
     end
 
   end
-  
+
   protected
-  
-  def default_routes(env, params) 
-    
+
+  def default_routes(env, params)
+
     log = @log
-    
+
     get '/login2/*' do
       params[:splat].inspect
       redirect '/login' + params[:splat].first
     end
-    
-    get '/login/*' do      
+
+    get '/login/*' do
       url = params[:splat].any? ? params[:splat][0][/(?<=referer=).*/] : '/'
-      login_form(referer: url)      
-    end    
-    
+      login_form(referer: url)
+    end
+
     post '/login' do
-      
+
       h = @req.params
 
       if @passwords[h['username']] == h['password'] then
-        
+
         @req.session[:username] = h['username']
         #'you are now logged in as ' + h['username']
         redirect h['referer']
-        
+
       else
-        
+
         login_form('Invalid username or password, try again.',401)
-        
+
       end
 
-      
+
     end
-        
+
     get '/logout' do
-      
+
       @req.session.clear
       'you are now logged out'
-      
-    end       
-    
+
+    end
+
     get '/session' do
-      
+
       #@req.session.expires
       #@req.session.options[:expire_after] = 1
       @req.session.options.inspect
-      
+
     end
-    
+
     get '/user' do
-      
+
       if @req.session[:username] then
         'You are ' + @req.session[:username]
       else
         'you need to log in to view this page'
-      end      
-      
+      end
+
     end
 
     get '/unauthorised' do
-      ['unauthorised user', 'text/plain', 403]      
+      ['unauthorised user', 'text/plain', 403]
     end
-    
-    super(env, params)   
- 
-  end   
-  
+
+    super(env, params)
+
+  end
+
   def login_form(msg='Log in to this site.', http_code=200, referer: '/')
-    
-s=<<EOF      
+
+s=<<EOF
 p #{msg}
 
 login
